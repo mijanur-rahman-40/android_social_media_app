e467127248a0360b2531205c4f000935d2e545d9
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-02-19 22:20:59 +0600

    Some files updated

---
+++ app/src/main/java/com/example/social_media_app/ViewsModel/BottomMenuFragments/ChatListFragment.java
@@ -0,0 +1,99 @@
+package com.example.social_media_app.ViewsModel.BottomMenuFragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.example.social_media_app.R;
+import com.example.social_media_app.Views.MainActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+
+public class ChatListFragment extends Fragment {
+
+
+    FirebaseAuth firebaseAuth;
+
+    public ChatListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+
+        // init
+        firebaseAuth = FirebaseAuth.getInstance();
+        return view;
+    }
+
+    private void checkUserStatus() {
+        // get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            // user is signed in stay here
+            // set email of logged in user
+            // emailText.setText(user.getEmail());
+        } else {
+            // user not sign in, go to main activity
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            Objects.requireNonNull(getActivity()).finish();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true); // to show menu option in fragment
+        super.onCreate(savedInstanceState);
+    }
+
+    // inflate option menu
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater menuInflater) {
+        // inflate menu_main
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        // hide add post icon from this fragment
+        menu.findItem(R.id.actionAddPost).setVisible(false);
+
+
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
+
+
+    // handle menu item click
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // get item id
+        int id = item.getItemId();
+        if (id == R.id.actionLogout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
